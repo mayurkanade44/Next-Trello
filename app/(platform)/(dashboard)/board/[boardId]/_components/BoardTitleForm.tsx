7ddcf6a2e77709c9b01110be3f48f50888f50985
mayurkanade44 <mayurkanade44@gmail.com>
2024-01-09 16:46:46 +0530

    update the board title

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardTitleForm.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import FormInput from "@/components/form/FormInput";
+import { Button } from "@/components/ui/button";
+import { Board } from "@prisma/client";
+import { ElementRef, useRef, useState } from "react";
+import { updateBoard } from "@/actions/updateBoard";
+import { useAction } from "@/hooks/useAction";
+import { toast } from "sonner";
+
+interface BoardTitleFormProps {
+  data: Board;
+}
+
+const BoardTitleForm = ({ data }: BoardTitleFormProps) => {
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board ${data.title} updated`);
+      setTitle(data.title);
+      disableEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+  const [isEditing, setEditing] = useState(false);
+  const [title, setTitle] = useState(data.title);
+
+  const enableEditing = () => {
+    setEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.select();
+    });
+  };
+
+  const disableEditing = () => {
+    setEditing(false);
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    execute({ title, id: data.id });
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
+  if (isEditing) {
+    return (
+      <form
+        action={onSubmit}
+        ref={formRef}
+        className="flex items-center gap-x-2"
+      >
+        <FormInput
+          ref={inputRef}
+          id="title"
+          onBlur={onBlur}
+          defaultValue={title}
+          className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none"
+        />
+      </form>
+    );
+  }
+  return (
+    <Button
+      onClick={enableEditing}
+      variant="transparent"
+      className="font-bold text-lg h-auto w-auto p-1 px-2"
+    >
+      {title}
+    </Button>
+  );
+};
+export default BoardTitleForm;
