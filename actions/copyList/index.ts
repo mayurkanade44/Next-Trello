e1c90c2ab850d3de22818f1c684480a52d9a1b41
mayurkanade44 <mayurkanade44@gmail.com>
2024-01-10 16:06:30 +0530

    copy list added

---
+++ actions/copyList/index.ts
@@ -0,0 +1,74 @@
+"use server";
+
+import { createSafeAction } from "@/lib/createSafeAction";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { CopyList } from "./schema";
+import { InputType, ReturnType } from "./types";
+import { redirect } from "next/navigation";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id, boardId } = data;
+  let list;
+
+  try {
+    const copyList = await db.list.findUnique({
+      where: {
+        id,
+        boardId,
+        board: { orgId },
+      },
+      include: {
+        cards: true,
+      },
+    });
+
+    if (!copyList) return { error: "List not found" };
+
+    const lastList = await db.list.findFirst({
+      where: { boardId },
+      orderBy: { order: "desc" },
+      select: { order: true },
+    });
+
+    const newOrder = lastList ? lastList.order + 1 : 1;
+
+    list = await db.list.create({
+      data: {
+        title: `${copyList.title} - Copy`,
+        boardId: copyList.boardId,
+        order: newOrder,
+        cards: {
+          createMany: {
+            data: copyList.cards.map((card) => ({
+              title: card.title,
+              description: card.description,
+              order: card.order,
+            })),
+          },
+        },
+      },
+      include: {
+        cards: true,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to copy list",
+    };
+  }
+
+  revalidatePath(`/organization/${boardId}`);
+  return { data: list };
+};
+
+export const copyList = createSafeAction(CopyList, handler);
--- app/(platform)/(dashboard)/board/[boardId]/_components/ListOptions.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListOptions.tsx
@@ -15,6 +15,7 @@ import { useAction } from "@/hooks/useAction";
 import { deleteList } from "@/actions/deleteList";
 import { toast } from "sonner";
 import { ElementRef, useRef } from "react";
+import { copyList } from "@/actions/copyList";
 
 interface ListOptionsProps {
   data: List;
@@ -34,11 +35,28 @@ const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
     },
   });
 
+  const { execute: executeCopy } = useAction(copyList, {
+    onSuccess: (data) => {
+      toast.success(`List ${data.title} copied`);
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
   const onDelete = (formData: FormData) => {
     const id = formData.get("id") as string;
     const boardId = formData.get("boardId") as string;
 
     executeDelete({ id, boardId });
+  };
+
+  const onCopy = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeCopy({ id, boardId });
   };
 
   return (
@@ -67,7 +85,7 @@ const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
         >
           Add card...
         </Button>
-        <form>
+        <form action={onCopy}>
           <input hidden name="id" id="id" value={data.id} />
           <input hidden name="boardId" id="boardId" value={data.boardId} />
           <FormSubmit
